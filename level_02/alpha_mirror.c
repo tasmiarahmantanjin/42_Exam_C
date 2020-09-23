853976ec11184021250ee2be4a3f2e255e6336e0
Tasmia Rahman <trahman@c0r1p10.hive.fi>
2020-09-23 09:48:19 +0300

    solution of alpha mirror

--- level_02/alpha_mirror.c
+++ level_02/alpha_mirror.c
@@ -6,16 +6,29 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 14:02:52 by trahman           #+#    #+#             */
-/*   Updated: 2020/09/23 09:28:46 by trahman          ###   ########.fr       */
+/*   Updated: 2020/09/23 09:47:44 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(int argv, char **argc)
+int	main(int argc, char **argv)
 {
 	int i;
 
 	i = 0;
-	
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			if(argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				argv[1][i] = 'Z' - argv[1][i] + 'A';
+			if(argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = 'z' - argv[1][i] + 'a';
+			write(1, &argv[1][i], 1);
+				i++;
+		}
+	}
+	write(1, "\n", 1);
+	return(0);
 }
