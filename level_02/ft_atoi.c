7bd2df78409e58bef1d162db165ed76ccc8e0bcd
Tasmia Rahman <tasmiata@gmail.com>
2020-09-29 10:52:06 +0300

    solution added

---
+++ level_02/ft_atoi.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/29 09:56:11 by trahman           #+#    #+#             */
+/*   Updated: 2020/09/29 10:51:14 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(char *str)
+{
+	int result;
+	int sign;
+
+	result = 0;
+	sign = 1;
+	if (!str)
+		return (0);
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+//	while (*str == '\t' || *str == '\v' || *str == '\n'
+//			|| *str == '\r' || *str == '\f' || *str == ' ')
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
+
+int main()
+{
+//	printf("ft_atoi: %d\n", ft_atoi("123456"));
+//	printf("atoi: %d\n", atoi("123456"));
+//	printf("ft_atoi: %d\n", ft_atoi("12Three45678"));
+//	printf("atoi: %d\n", atoi("12Three45678"));
+//	printf("ft_atoi: %d\n", ft_atoi("Hello World!"));
+//	printf("atoi: %d\n", atoi("Hello World!"));
+//	printf("ft_atoi: %d\n", ft_atoi("+42 BLAH!"));
+//	printf("atoi: %d\n", atoi("+42 BLAH!"));
+//	printf("ft_atoi: %d\n", ft_atoi("-42"));
+	printf("atoi: %d\n", atoi("-42"));
+	printf("ft_atoi: %d\n", ft_atoi("     +42"));
+	printf("atoi: %d\n", atoi("     +42"));
+	printf("ft_atoi: %d\n", ft_atoi("\t\n\v\f\r 42"));
+	printf("atoi: %d\n", atoi("\t\n\v\f\r 42"));
+
+	return 0;
+}
