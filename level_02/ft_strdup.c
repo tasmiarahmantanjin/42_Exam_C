4f6167490f141dc66a6eaefa4f4ce8845c0fafcd
Tasmia Rahman <tasmiata@gmail.com>
2020-09-29 10:58:48 +0300

    solution added with main.c

--- level_02/ft_strdup.c
+++ level_02/ft_strdup.c
@@ -6,12 +6,13 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 09:12:45 by trahman           #+#    #+#             */
-/*   Updated: 2020/09/29 09:43:46 by trahman          ###   ########.fr       */
+/*   Updated: 2020/09/29 10:58:35 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strdup(char *src)
 {
@@ -39,9 +40,12 @@ int	main(void)
 {
 	char	*str;
 	char	*str2;
+	char	*str3;
 	
 	str = "42 school";
 	str2 = ft_strdup(str);
+	str3 = strdup(str);
 	printf("%s\n", str);
 	printf("%s\n", str2);
+	printf("%s\n", str3);
 }
