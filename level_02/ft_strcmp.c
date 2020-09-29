c64e83c85cfd4354f53f5b87ae7fafdc40172db6
Tasmia Rahman <tasmiata@gmail.com>
2020-09-29 09:54:49 +0300

    updated solution

--- level_02/ft_strcmp.c
+++ level_02/ft_strcmp.c
@@ -6,16 +6,31 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 22:56:48 by trahman           #+#    #+#             */
-/*   Updated: 2020/09/28 22:56:51 by trahman          ###   ########.fr       */
+/*   Updated: 2020/09/29 09:54:32 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(char *s1, char *s2)
+#include <stdio.h>
+
+int ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	int i;
+	i = 0;
+
+	while (s1[i])
 	{
-		s1 += 1;
-		s2 += 2;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (*s1 - *s2);
+	return (s1[i] - s2[i]);
+}
+
+int main()
+{
+char str1[] = "Tasmia";
+char str2[] = "Tasmia";
+
+printf("%d\n", ft_strcmp(str1, str2));
+return(0);
 }
