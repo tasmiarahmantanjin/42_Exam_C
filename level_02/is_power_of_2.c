a5b17417577c5fcb76217c6efdc63e83a0d0bde5
Tasmia Rahman <tasmiata@gmail.com>
2020-09-29 12:14:50 +0300

    solution with main

--- level_02/is_power_of_2.c
+++ level_02/is_power_of_2.c
@@ -6,11 +6,39 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 22:45:35 by trahman           #+#    #+#             */
-/*   Updated: 2020/09/28 22:47:20 by trahman          ###   ########.fr       */
+/*   Updated: 2020/09/29 12:14:04 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_power_of_2(unsigned int n)
+#include <stdio.h>
+
+int is_power_of_2(unsigned int n)
 {
-	return ((n & (-n)) == n ? 1 : 0);
+	if (n == 0)
+		return (0);
+	else
+		return ((n & (-n)) == n ? 1: 0);
+}
+
+int main(void)
+{
+	unsigned int num[7];
+	num[0] = 0;
+	num[1] = 200;
+	num[2] = 32;
+	num[3] = 256;
+	num[4] = 13;
+	num[5] = 1000;
+	num[6] = 1024;
+	int i;
+
+	i = 0;
+	while(i <= 6)
+	{
+	if (is_power_of_2(num[i]))
+		printf("%s %d\n", "yep", num[i]);
+	else
+		printf("%s %d\n", "nope", num[i]);
+	i++;
+	}
 }
