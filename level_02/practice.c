cb83db6197881fc03f77685346d338d677f909fc
Tasmia Rahman <tasmiata@gmail.com>
2020-09-29 12:30:56 +0300

    practice

--- level_02/practice.c
+++ level_02/practice.c
@@ -6,7 +6,7 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 12:05:29 by trahman           #+#    #+#             */
-/*   Updated: 2020/09/29 12:10:02 by trahman          ###   ########.fr       */
+/*   Updated: 2020/09/29 12:29:41 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,30 @@ unsigned char	reverse_bits(unsigned char octet)
 unsigned char	swap_bits(unsigned char c)
 {
 	return ((c >> 4) | (c << 4));
+}
+
+int is_power_of_2(unsigned int n)
+{
+	if (n == 0)
+		return (0);
+	else
+		return ((n & (-n)) == n ? 1: 0);
+}
+
+char	*ft_strrev(char *str)
+{
+	int		i;
+	int		len;
+	char	temp;
+
+	i = -1;
+	while (str[len])
+		len++;
+	while (++i < --len)
+	{
+		temp = str[i];
+		str[i] = str[len];
+		str[len] = temp;
+	}
+	return (str);
 }
