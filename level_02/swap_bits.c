c6f187ab6f83704210e35f30b921de166bac3972
Tasmia Rahman <tasmiata@gmail.com>
2020-09-29 12:04:48 +0300

    solution with main

--- level_02/swap_bits.c
+++ level_02/swap_bits.c
@@ -6,11 +6,27 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 22:40:09 by trahman           #+#    #+#             */
-/*   Updated: 2020/09/28 22:45:07 by trahman          ###   ########.fr       */
+/*   Updated: 2020/09/29 12:04:15 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 unsigned char	swap_bits(unsigned char c)
 {
 		return ((c >> 4) | (c << 4));
+}
+
+int main(void)
+{
+	char letter_t;
+	char letter_G;
+	letter_t = 't';
+	letter_G = 'G';
+
+	printf("letter_t after swap: %c", swap_bits(letter_t));
+	printf("\n");
+	printf("letter_G after swap: %c", swap_bits(letter_G));
+	printf("\n");
+	return (0);
 }
