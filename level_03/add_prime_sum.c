e1147a99904a16246243ef45516207f9799397d2
Tasmia Rahman <tasmiata@gmail.com>
2020-10-05 09:23:25 +0300

    add prime sum

--- level_03/add_prime_sum.c
+++ level_03/add_prime_sum.c
@@ -6,7 +6,7 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 09:08:01 by trahman           #+#    #+#             */
-/*   Updated: 2020/10/05 09:13:51 by trahman          ###   ########.fr       */
+/*   Updated: 2020/10/05 09:23:18 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,28 @@
 
 int	ft_atoi(char *str)
 {
-	
+	int	sign;
+	int	result;
+
+	result = 0;
+	sign = 1;
+	if (!str)
+		return (0);
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		*str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = (result * 10) + (*str) - '0';
+		str++;
+	}
+	return (sign * result);
 }
 
+void	ft_putnbr(int	nb)
+{
+	
+}
