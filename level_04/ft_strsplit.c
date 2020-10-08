dc3bcf28e4d0bd4562257f72ba53db49d7499aa8
Tasmia Rahman <trahman@c0r1p10.hive.fi>
2020-10-08 12:00:03 +0300

    first version of ft_strsplit

---
+++ level_04/ft_strsplit.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/08 11:55:29 by trahman           #+#    #+#             */
+/*   Updated: 2020/10/08 11:59:25 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+/*
+** The ft_chr() function will read and increase the charecter from input string
+** if the the charecter is not the delimeter than it will increase the as i++
+** and return the index number of 2nd layer array
+*/
+
+static	int		ft_chr(char const *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+int	ft_wordcount(char const *s, char c)
+{
+	int i;
+	int count;
+
+	i = 0;
+	while (s[i] && s[i] == c)
+		i++;
+	if (s[i] == '\0')
+		return (0);
+	count = 1;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	char	**ptr;
+	int		x;
+	int		y;
+
+	if (!s)
+		return (0);
+	if (!(ptr = (char **)malloc(sizeof(char *) * (ft_wordcount(s, c) + 1))))
+		return (NULL);
+	x = -1;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			if (!(ptr[++x] = (char *)malloc(sizeof(char) * (ft_chr(s, c) + 1))))
+				return (NULL);
+			y = 0;
+			while (*s && *s != c)
+				ptr[x][y++] = *s++;
+			ptr[x][y] = '\0';
+		}
+	}
+	ptr[++x] = NULL;
+	return (ptr);
+}
+
