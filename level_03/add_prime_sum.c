fc5e19832c2577092952015ca592e4aa73c93dfe
Tasmia Rahman <tasmiata@gmail.com>
2020-10-12 18:46:44 +0300

    solution of add_prime_sum

--- level_03/add_prime_sum.c
+++ level_03/add_prime_sum.c
@@ -6,7 +6,7 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 09:08:01 by trahman           #+#    #+#             */
-/*   Updated: 2020/10/05 09:23:18 by trahman          ###   ########.fr       */
+/*   Updated: 2020/10/12 18:44:19 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_atoi(char *str)
 	if (!str)
 		return (0);
 	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		*str++;
+		str++;
 	if (*str == '-')
 		sign = -1;
 	if (*str == '-' || *str == '+')
@@ -35,7 +35,58 @@ int	ft_atoi(char *str)
 	return (sign * result);
 }
 
-void	ft_putnbr(int	nb)
+void	ft_putnbr(int nb)
 {
-	
+	char	c;
+
+	if (nb < 0)
+	{
+		nb = -nb;
+		write(1, "-", 1);
+	}
+	if (nb < 10)
+	{
+		c = nb + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+int		is_prime(int nb)
+{
+	int i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= (nb / 2))
+	{
+		if (!(nb % i))
+			return (0);
+		else
+			i += 1;
+	}
+	return (1);
+}
+
+int		main(int argc, char *argv[])
+{
+	int		nb;
+	int		sum;
+
+	if (argc == 2)
+	{
+		nb = ft_atoi(argv[1]);
+		sum = 0;
+		while (nb > 0)
+			if (is_prime(nb--))
+				sum += (nb + 1);
+		ft_putnbr(sum);
+	}
+	write(1, "\n", 1);
+	return (0);
 }
