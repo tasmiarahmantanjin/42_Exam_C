5411dea53f774e6639116698a11a30130ffc3c60
Tasmia Rahman <tasmiata@gmail.com>
2020-10-12 21:03:44 +0300

    solution added

---
+++ level_03/pgcd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pgcd.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/12 20:53:20 by trahman           #+#    #+#             */
+/*   Updated: 2020/10/12 21:03:24 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		main(int argc, char **argv)
+{
+	int		nbr1;
+	int		nbr2;
+
+	nbr1 = atoi(argv[1]);
+	nbr2 = atoi(argv[2]);
+
+	if (argc == 3)
+	{
+		if ((nbr1 > 0) && (nbr2 > 0))
+		{
+			while (nbr1 != nbr2)
+			{
+				if (nbr1 > nbr2)
+					nbr1 = nbr1 - nbr2;
+				else
+					nbr2 = nbr2 - nbr1;
+			}
+			printf("%d", nbr1);
+		}
+	}
+	printf("\n");
+	return (0);
+}
