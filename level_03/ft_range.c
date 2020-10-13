78a1ac12b55f3a07eb3007509ce9b0f7150722c4
Tasmia Rahman <tasmiata@gmail.com>
2020-10-13 11:19:40 +0300

    solution with main

---
+++ level_03/ft_range.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/13 10:32:08 by trahman           #+#    #+#             */
+/*   Updated: 2020/10/13 11:19:06 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int start, int end)
+{
+	int	*range;
+	int	i;
+
+	i = 0;
+	if (start > end)
+		range = (int *)malloc(sizeof(int) * (start - end) + 1);
+	else
+		range = (int *)malloc(sizeof(int) * (end - start) + 1);
+	while (start != end)
+	{
+		range[i++] = start;
+		start += (start > end) ? -1 : 1;
+	}
+	range[i] = start;
+	return (range);
+}
+
+int	main(void)
+{
+	int i;
+	int *result;
+
+	i = 0;
+	result = ft_range(1, 4);
+	while (i < 4)
+	{
+		printf("The value is %d\n", result[i]);
+		i++;
+	}
+	return (0);
+}
