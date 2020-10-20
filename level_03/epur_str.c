fba64d42ff9c33856b6da516257df8f583897f10
Tasmia Rahman <tasmiata@gmail.com>
2020-10-20 12:07:36 +0300

    updated solution

--- level_03/epur_str.c
+++ level_03/epur_str.c
@@ -6,7 +6,7 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 18:50:32 by trahman           #+#    #+#             */
-/*   Updated: 2020/10/12 20:07:52 by trahman          ###   ########.fr       */
+/*   Updated: 2020/10/20 12:07:14 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 int		main(int argc, char const **argv)
 {
-	int		i;
-	int		flg;
+	int	i;
+	int	flg;
 
+	i = 0;
 	if (argc == 2)
 	{
-		i = 0;
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i += 1;
+		while ((argv[1][i] == ' ') || (argv[1][i]== '\t'))
+			i++;
 		while (argv[1][i])
 		{
-			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+			if ((argv[1][i] == ' ') || (argv[1][i]== '\t'))
 				flg = 1;
-			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+			if (!(argv[1][i] == ' ') || (argv[1][i]== '\t'))
 			{
-				if (flg)
+				if(flg)
 					write(1, " ", 1);
 				flg = 0;
 				write(1, &argv[1][i], 1);
 			}
-			i += 1;
+			i++;
 		}
 	}
 	write(1, "\n", 1);
