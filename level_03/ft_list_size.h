3dd09c439c0982a6f44d72248ae46bda2b1d03e0
Tasmia Rahman <tasmiata@gmail.com>
2020-10-20 16:28:28 +0300

    solution with main

--- level_03/ft_list_size.h
+++ level_03/ft_list_size.h
@@ -6,17 +6,17 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 13:00:05 by trahman           #+#    #+#             */
-/*   Updated: 2020/10/13 13:00:31 by trahman          ###   ########.fr       */
+/*   Updated: 2020/10/20 16:13:44 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LIST_SIZE_H
 # define FT_LIST_SIZE_H
 
-typedef struct    s_list
+typedef struct		s_list
 {
-	struct s_list *next;
-	void          *data;
-}                 t_list;
+	struct s_list	*next;
+	void			*data;
+}					t_list;
 
 #endif
