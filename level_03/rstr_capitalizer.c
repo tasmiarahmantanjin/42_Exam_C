19590eda18aeaaff6dbb99f266cce5b60231082d
Tasmia Rahman <tasmiata@gmail.com>
2020-10-20 12:06:13 +0300

    updated spultion

--- level_03/rstr_capitalizer.c
+++ level_03/rstr_capitalizer.c
@@ -6,7 +6,7 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 20:53:15 by trahman           #+#    #+#             */
-/*   Updated: 2020/10/19 20:58:28 by trahman          ###   ########.fr       */
+/*   Updated: 2020/10/20 12:02:11 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,34 @@
 
 void	rstr_capitalizer(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i])
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] += 32;
-		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
-			|| str[i + 1] == '\t' || str[i + 1] == '\0'))
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
 			str[i] -= 32;
 		write(1, &str[i++], 1);
 	}
 }
 
-int main(int ac, char **av)
+
+int main(int argc, char **argv)
 {
 	int i;
 
-	if (ac < 1)
+	if (argc < 1)
 		write(1, "\n", 1);
 	else
 	{
 		i = 1;
-		while (i < ac)
+		while (i < argc)
 		{
-			rstr_capitalizer(av[i]);
+			rstr_capitalizer(argv[i]);
 			write(1, "\n", 1);
-			i += 1;
+			i++;
 		}
 	}
 	return (0);
