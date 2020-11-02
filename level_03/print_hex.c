1d4b3b9126fff4c777c431c7e2072d7c3837e8a1
Tasmia Rahman <tasmiata@gmail.com>
2020-11-02 14:59:50 +0200

    bug fix

--- level_03/print_hex.c
+++ level_03/print_hex.c
@@ -6,33 +6,39 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 22:09:43 by trahman           #+#    #+#             */
-/*   Updated: 2020/10/04 22:10:12 by trahman          ###   ########.fr       */
+/*   Updated: 2020/11/02 14:59:30 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	print_hex(int p)
+void	print_hex(int p) // driver function.
 {
-	char *str;
-
-	str = "0123456789abcdef";
-	if (p == 0)
-		write (1, "0", 1);
-	while (p)
-	{
-		write(1, &str[p % 16], 1);
-		p /= 16;
-	}
+	char	*str;
+	
+	str = "0123456789abcdef"; // base array to find all the hexa values.
+	if (p >= 16)
+		print_hex(p / 16); // recursive call if number is greater then base.
+	write (1, &str[p % 16], 1);
 }
 
-int	ft_atoi(char *str)
+/* Another solution of print_hex function
+void	print_hex(int n)
 {
-	int	result;
-	int	sign;
+	if (n >= 16)
+		print_hex(n / 16);
+	n = n % 16;
+	n += n < 10 ? '0' : 'a' - 10;
+	write(1, &n, 1);
+} */
 
-	result = 0;
+int		ft_atoi(char *str)
+{
+	int sign;
+	int result;
+
 	sign = 1;
+	result = 0;
 	if (!str)
 		return (0);
 	while (*str == ' ' || (*str >= 9 && *str <= 13))
