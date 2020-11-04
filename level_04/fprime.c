b1037bf49be8a357e86952494e7c6e7147025bed
Tasmia Rahman <tasmiata@gmail.com>
2020-11-04 10:20:31 +0200

    solution of fprime

---
+++ level_04/fprime.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 18:09:14 by trahman           #+#    #+#             */
+/*   Updated: 2020/11/03 18:30:18 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+	int		nbr;
+
+	if (argc == 2)
+	{
+		i = 2;
+		nbr = atoi(argv[1]);
+		if (nbr == 1)
+			printf("1");
+		while (nbr >= i)
+		{
+			if (nbr % i == 0)   //nbr = 15
+			{
+				printf("%d", i);
+				if (nbr == i)
+					break ;
+				printf("*");
+				nbr /= i; // nbr = nbr / i(to save the reminder)
+				i = 1;
+			}
+			i++;
+		}
+	}
+	printf("\n");
+	return (0);
+}
