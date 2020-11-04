ddba2bfaff1ca6c3b673a611e5f77c9a00e9ec33
Tasmia Rahman <tasmiata@gmail.com>
2020-11-04 10:21:05 +0200

    solution of sort_int_tab

---
+++ level_04/sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 18:33:23 by trahman           #+#    #+#             */
+/*   Updated: 2020/11/03 18:39:15 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	int				tmp;
+	unsigned int 	i;
+	unsigned int 	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j += 1;
+		}
+		i += 1;
+	}
+}
