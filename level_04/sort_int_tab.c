c206aa6a608fcfaabbd339d99c79ed8d38b774f0
Tasmia Rahman <tasmiata@gmail.com>
2020-11-06 14:41:37 +0200

    norm check

--- level_04/sort_int_tab.c
+++ level_04/sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 18:33:23 by trahman           #+#    #+#             */
-/*   Updated: 2020/11/04 10:34:38 by trahman          ###   ########.fr       */
+/*   Updated: 2020/11/06 14:40:22 by trahman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,9 @@ void	sort_int_tab(int *tab, unsigned int size)
 
 int		main(void)
 {
-	int a[6] = {8, 7, 6, 1, 5, 10};
-	int i = 0;
-	int size = 6;
+	int	a[6] = {8, 7, 6, 1, 5, 10};
+	int	i = 0;
+	int	size = 6;
 
 	sort_int_tab(a, size);
 	while (i < size)
